5d7b4972ff254291f26382669c35d79efa3f0bb7
KCAA Dev <kcaa.dev@gmail.com>
2014-02-10 06:37:30 +0900

    Use a proxy for browsers.

--- server/server_main.py
+++ server/server_main.py
@@ -8,14 +8,17 @@ from selenium import webdriver
 
 def parse_args(argv):
     # TODO: Serve these files from this server.
-    url_default = 'http://localhost:3030/kcaa/client/web/index.html'
+    url_default = 'http://127.0.0.1:3030/kcaa/client/web/index.html'
 
     parser = argparse.ArgumentParser(description='KCAA server.')
     parser.add_argument('--url', default=url_default,
                         help='Url to open.')
-    parser.add_argument('--proxy_controller', default='http://localhost:9090',
+    parser.add_argument('--proxy_controller', default='localhost:9090',
                         help='Proxy controller address.')
-    parser.add_argument('--browser', default='chrome', choices=['chrome'],
+    parser.add_argument('--proxy', default='localhost:9091',
+                        help='Proxy address.')
+    parser.add_argument('--browser', default='chrome',
+                        choices=['chrome', 'firefox'],
                         help='Browser to use.')
     parser.add_argument('--chrome_binary', default='/usr/bin/google-chrome',
                         help='Chrome binary to use. Useful especially when '
@@ -26,20 +29,40 @@ def parse_args(argv):
 
 
 def setup(args):
+    desired_capabilities = get_desired_capabilities(args)
     browser = None
     if args.browser == 'chrome':
-        browser = setup_chrome(args)
+        browser = setup_chrome(args, desired_capabilities)
+    elif args.browser == 'firefox':
+        browser = setup_firefox(args, desired_capabilities)
     else:
         raise ValueError('Unrecognized browser: {browser}'.format(
             browser=args.browser))
     browser.get(args.url)
 
 
-def setup_chrome(args):
+def get_desired_capabilities(args):
+    capabilities = {}
+    capabilities['proxy'] = {'httpProxy': args.proxy,
+                             'ftpProxy': args.proxy,
+                             'sslProxy': args.proxy,
+                             'noProxy': None,
+                             'proxyType': 'MANUAL',
+                             'class': 'org.openqa.selenium.Proxy',
+                             'autodetect': False}
+    return capabilities
+
+
+def setup_chrome(args, desired_capabilities):
     options = webdriver.ChromeOptions()
     options.binary_location = args.chrome_binary
     return webdriver.Chrome(executable_path=args.chromedriver_binary,
-                            chrome_options=options)
+                            chrome_options=options,
+                            desired_capabilities=desired_capabilities)
+
+
+def setup_firefox(args, desired_capabilities):
+    return webdriver.Firefox(capabilities=desired_capabilities)
 
 
 def main(argv):
--- tools/start_game.sh
+++ tools/start_game.sh
@@ -5,7 +5,8 @@ source $(dirname $0)/config
 SERVER_BIN=$(dirname $0)/../server/server_main.py
 
 ${SERVER_BIN} \
-  --proxy_controller=http://${PROXY_CONTROLLER_HOST}:${PROXY_CONTROLLER_PORT} \
+  --proxy_controller=${PROXY_CONTROLLER_HOST}:${PROXY_CONTROLLER_PORT} \
+  --proxy=${PROXY_CONTROLLER_HOST}:${PROXY_PORT} \
   --browser=${BROWSER} \
   --chrome_binary=${CHROME_BIN} \
   --chromedriver_binary=${CHROMEDRIVER_BIN}
