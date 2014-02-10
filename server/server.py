f04b85d6536cdfcf3d51e237bde7c2e63a5c2946
KCAA Dev <kcaa.dev@gmail.com>
2014-02-10 10:33:46 +0900

    Handle only /client requests to file serving.

--- server/server.py
+++ server/server.py
@@ -4,19 +4,31 @@ import SocketServer
 
 class KcaaHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+    CLIENT_PREFIX = '/client/'
+
     def do_HEAD(self):
         # Note: HTTP request handlers are not new-style classes.
         # super() cannot be used.
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_HEAD(self)
+        if self.rewrite_to_client_path():
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_HEAD(self)
 
     def do_GET(self):
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        if self.rewrite_to_client_path():
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def rewrite_to_client_path(self):
+        if self.path.startswith(KcaaHTTPRequestHandler.CLIENT_PREFIX):
+            self.path = '/' + self.path[len(
+                KcaaHTTPRequestHandler.CLIENT_PREFIX):]
+            return True
+        else:
+            return False
 
 
 def setup(args):
     httpd = SocketServer.TCPServer(('', args.server_port),
                                    KcaaHTTPRequestHandler)
     _, port = httpd.server_address
-    root_url = 'http://127.0.0.1:{}/web/'.format(port)
+    root_url = 'http://127.0.0.1:{}/client/'.format(port)
     print 'KCAA server ready at {}'.format(root_url)
     return httpd, root_url
