c4acad425d88a9edb566d489978434f2e0006007
KCAA Dev <kcaa.dev@gmail.com>
2014-02-16 02:19:27 +0900

    Unify the name of properties in jsonobject_test.py.

--- server/kcaa/kcsapi/jsonobject_test.py
+++ server/kcaa/kcsapi/jsonobject_test.py
@@ -3,36 +3,37 @@
 import pytest
 
 import jsonobject
+from jsonobject import jsonproperty
 
 
 class TestJsonSerializableObject(object):
 
     def test_default_getter(self):
 
-        class DefaultGetter(jsonobject.JsonSerializableObject):
+        class SomeObject(jsonobject.JsonSerializableObject):
             # Can decorate without any arguments.
-            @jsonobject.jsonproperty
-            def field_foo(self):
-                return 'foo'
+            @jsonproperty
+            def foo(self):
+                return 'FOO'
 
-        g = DefaultGetter()
-        # field_foo can be used as a normal getter.
-        assert g.field_foo == 'foo'
+        s = SomeObject()
+        # "foo" can be used as a normal getter.
+        assert s.foo == 'FOO'
         # And is automatically exported.
-        assert g.json() == '{"field_foo": "foo"}'
+        assert s.json() == '{"foo": "FOO"}'
 
     def test_named_getter(self):
-        class NamedGetter(jsonobject.JsonSerializableObject):
-            # This field is exported as "FOO".
+        class SomeObject(jsonobject.JsonSerializableObject):
+            # This field is exported as "field_foo".
             # Note that parameters should be named; you can't do this way:
-            # @jsonobject.jsonproperty('FOO')
-            @jsonobject.jsonproperty(name='FOO')
-            def field_foo(self):
-                return 'foo'
+            # @jsonproperty('field_foo')
+            @jsonproperty(name='field_foo')
+            def foo(self):
+                return 'FOO'
 
-        g = NamedGetter()
-        assert g.field_foo == 'foo'
-        assert g.json() == '{"FOO": "foo"}'
+        s = SomeObject()
+        assert s.foo == 'FOO'
+        assert s.json() == '{"field_foo": "FOO"}'
 
     def test_not_exported_if_null(self):
         class SomeObject(jsonobject.JsonSerializableObject):
@@ -40,12 +41,12 @@ class TestJsonSerializableObject(object):
                 self.value = value
 
             # If store_if_null is False, the property will not exported.
-            # This is the default behavior.
-            @jsonobject.jsonproperty(store_if_null=False)
-            def field_foo(self):
+            # This is the default behavior if omitted.
+            @jsonproperty(store_if_null=False)
+            def foo(self):
                 return self.value
 
-        assert SomeObject('foo').json() == '{"field_foo": "foo"}'
+        assert SomeObject('FOO').json() == '{"foo": "FOO"}'
         assert SomeObject(None).json() == '{}'
 
     def test_exported_if_null(self):
@@ -54,99 +55,111 @@ class TestJsonSerializableObject(object):
                 self.value = value
 
             # If store_if_null is True, the property will always be exported.
-            @jsonobject.jsonproperty(store_if_null=True)
-            def field_foo(self):
+            @jsonproperty(store_if_null=True)
+            def foo(self):
                 return self.value
 
-        assert SomeObject('foo').json() == '{"field_foo": "foo"}'
-        assert SomeObject(None).json() == '{"field_foo": null}'
+        assert SomeObject('FOO').json() == '{"foo": "FOO"}'
+        assert SomeObject(None).json() == '{"foo": null}'
 
-    def test_non_primitive_value_getter(self):
+    def test_non_primitive_value(self):
         class NonPrimitiveObject(object):
             pass
 
-        class NonPrimitiveValueGetter(jsonobject.JsonSerializableObject):
-            # The getter cannot return a non primitive value.
-            @jsonobject.jsonproperty
-            def field_foo(self):
+        class SomeObject(jsonobject.JsonSerializableObject):
+            # The getter cannot return a non JSON primitive value.
+            @jsonproperty
+            def foo(self):
                 return NonPrimitiveObject()
 
         with pytest.raises(TypeError):
-            NonPrimitiveValueGetter().json()
+            SomeObject().json()
 
     def test_non_primitive_serializable_value_getter(self):
         class NonPrimitiveSerializableObject(
                 jsonobject.JsonSerializableObject):
-            @jsonobject.jsonproperty
-            def field_child_foo(self):
-                return 'child_foo'
+            @jsonproperty
+            def child_foo(self):
+                return 'CHILD_FOO'
 
-        class NonPrimitiveSerializableValueGetter(
-                jsonobject.JsonSerializableObject):
+        class SomeObject(jsonobject.JsonSerializableObject):
             # OK if the non primitive value is JSON serializable.
-            @jsonobject.jsonproperty
-            def field_foo(self):
+            @jsonproperty
+            def foo(self):
                 return NonPrimitiveSerializableObject()
 
-        assert (NonPrimitiveSerializableValueGetter().json() ==
-                '{"field_foo": {"field_child_foo": "child_foo"}}')
+        assert SomeObject().json() == '{"foo": {"child_foo": "CHILD_FOO"}}'
 
-    def test_getter_setter_deleter(self):
-        class GetterSetterDeleter(jsonobject.JsonSerializableObject):
+    def test_setter_and_deleter(self):
+        class SomeObject(jsonobject.JsonSerializableObject):
             def __init__(self, foo):
                 self._foo = foo
 
-            @jsonobject.jsonproperty
-            def field_foo(self):
+            @jsonproperty
+            def foo(self):
                 return self._foo
 
-            # Setters and deleters can be used just like the standard property.
-            @field_foo.setter
-            def field_foo(self, foo):
+            # Setter can be used just like the standard property.
+            @foo.setter
+            def foo(self, foo):
                 self._foo = foo
 
-            @field_foo.deleter
-            def field_foo(self):
+            # The same for deleter. You should reset the value before caalling
+            # json(), though.
+            @foo.deleter
+            def foo(self):
                 del self._foo
 
-        gsd = GetterSetterDeleter(123)
-        assert gsd.field_foo == 123
-        assert gsd.json() == '{"field_foo": 123}'
-        gsd.field_foo = 456
-        assert gsd.field_foo == 456
-        assert gsd.json() == '{"field_foo": 456}'
-        del gsd.field_foo
+        s = SomeObject(123)
+        assert s.foo == 123
+        assert s.json() == '{"foo": 123}'
+        s.foo = 456
+        assert s.foo == 456
+        assert s.json() == '{"foo": 456}'
+        del s.foo
+        # Accessing a deleted property will raise AttributeError.
         with pytest.raises(AttributeError):
-            assert gsd.field_foo
+            s.foo
+        # And trying to convert the container object to JSON will raise
+        # TypeError.
+        with pytest.raises(TypeError):
+            s.json()
+        # Resetting the property will make the conversion possible again.
+        s.foo = 789
+        assert s.json() == '{"foo": 789}'
 
     def test_json_property(self):
-        class SuccinctProperty(jsonobject.JsonSerializableObject):
-            # This does the same thing as @jsonproperty getter and setter.
-            field_foo = jsonobject.JsonProperty('field_foo')
+        class SomeObject(jsonobject.JsonSerializableObject):
+            # This does the same thing as @jsonproperty getter, setter and
+            # deleter.
+            # Note that the JSON name is mandatory. It can be a positional
+            # argument.
+            foo = jsonobject.JsonProperty('foo')
 
-        sp = SuccinctProperty()
+        s = SomeObject()
         # The property defaults to None.
-        assert sp.field_foo is None
-        sp.field_foo = 123
-        assert sp.field_foo == 123
-        assert sp.json() == '{"field_foo": 123}'
-        del sp.field_foo
+        assert s.foo is None
+        s.foo = 'FOO'
+        assert s.foo == 'FOO'
+        assert s.json() == '{"foo": "FOO"}'
+        del s.foo
         with pytest.raises(AttributeError):
-            assert sp.field_foo
+            s.foo
 
     def test_json_readonly_property(self):
-        class ReadonlyProperty(jsonobject.JsonSerializableObject):
+        class SomeObject(jsonobject.JsonSerializableObject):
             def __init__(self, foo):
                 self._foo = foo
 
-            # This property export self._foo as a readonly property.
-            foo = jsonobject.ReadonlyJsonProperty('field_foo', '_foo')
+            # This property exports SomeObject._foo in a readonly manner.
+            foo = jsonobject.ReadonlyJsonProperty('foo', '_foo')
 
-        rp = ReadonlyProperty(123)
-        assert rp.foo == 123
+        s = SomeObject('FOO')
+        assert s.foo == 'FOO'
+        # Client code cannot assign a value to s.foo.
         with pytest.raises(AttributeError):
-            rp.foo = 456
-        assert rp.json('{"field_foo": 123}')
+            s.foo = 'BAR'
+        assert s.json('{"foo": "FOO"}')
 
 
 def main():
