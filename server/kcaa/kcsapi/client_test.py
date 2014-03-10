6e1892daec726b44b1bbb4d085e27fa03c0a419b
KCAA Dev <kcaa.dev@gmail.com>
2014-03-11 02:40:33 +0900

    Add a Screen test for sequence of KCSAPI responses.

--- server/kcaa/kcsapi/client_test.py
+++ server/kcaa/kcsapi/client_test.py
@@ -8,14 +8,40 @@ from kcaa import screens
 
 class TestScreen(object):
 
+    def update(self, screen, api_name):
+        screen.update(api_name, None, None, None, False)
+
+    def update_sequence(self, screen, api_names):
+        for api_name in api_names:
+            screen.update(api_name, None, None, None, False)
+
     def test_mission_result(self):
         screen = client.Screen()
         assert screen.screen == screens.UNKNOWN
-        screen.update('/api_get_member/deck_port', None, None, None, False)
+        self.update(screen, '/api_get_member/deck_port')
         assert screen.screen == screens.PORT
-        screen.update('/api_req_mission/result', None, None, None, False)
+        self.update(screen, '/api_req_mission/result')
         assert screen.screen == screens.MISSION_RESULT
-        screen.update('/api_get_member/deck_port', None, None, None, False)
+        self.update(screen, '/api_get_member/deck_port')
+        assert screen.screen == screens.MISSION_RESULT
+
+    def test_mission_result_real_sequence(self):
+        screen = client.Screen()
+        screen.screen = screens.PORT
+        self.update_sequence(screen, [
+            '/api_auth_member/logincheck',
+            '/api_get_member/material',
+            '/api_get_member/deck_port',
+            '/api_get_member/ndock',
+            '/api_get_member/ship3',
+            '/api_get_member/basic',
+            '/api_req_mission/result',
+            '/api_get_member/deck_port',
+            '/api_get_member/basic',
+            '/api_get_member/ship2',
+            '/api_get_member/material',
+            '/api_get_member/useitem',
+        ])
         assert screen.screen == screens.MISSION_RESULT
 
 
