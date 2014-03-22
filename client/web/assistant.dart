626de4d748bbe77bf1f996b87fa89401ec7daa78
KCAA Dev <kcaa.dev@gmail.com>
2014-03-22 23:49:25 +0900

    Add a modal dialog container.

--- client/web/assistant.dart
+++ client/web/assistant.dart
@@ -3,6 +3,7 @@ library kcaa;
 import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
+import 'package:bootjack/bootjack.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:polymer/polymer.dart';
@@ -91,6 +92,7 @@ class Assistant extends PolymerElement {
     // Theoretically this is not safe, as some data requiring ja_JP date format
     // may run before loading completes, but that would never happen in reality.
     initializeDateFormatting("ja_JP", null).then((_) => null);
+    Modal.use();
   }
 
   @override
