5e218cc2310172142aa2f05ca89de84fd33296da
KCAA Dev <kcaa.dev@gmail.com>
2014-03-30 18:21:25 +0900

    Add mission_complete field to Fleet.

--- server/kcaa/kcsapi/fleet.py
+++ server/kcaa/kcsapi/fleet.py
@@ -15,6 +15,10 @@ class Fleet(jsonobject.JSONSerializableObject):
     """IDs of ships belonging to this fleet."""
     mission_id = jsonobject.JSONProperty('mission_id', value_type=int)
     """ID of mission which this fleet is undertaking."""
+    mission_complete = jsonobject.JSONProperty('mission_complete',
+                                               value_type=bool)
+    """True if the mission is complete. Updated when the screen transitioned to
+    PORT_MAIN."""
 
 
 class FleetList(model.KCAAObject):
@@ -36,13 +40,17 @@ class FleetList(model.KCAAObject):
             for data in response['api_data']:
                 fleet_data = jsonobject.parse(data)
                 mission_id = None
+                mission_complete = None
                 if fleet_data.api_mission[0] != 0:
                     mission_id = fleet_data.api_mission[1]
+                    mission_complete = (
+                        True if fleet_data.api_mission[3] == 1 else False)
                 self.fleets.append(Fleet(
                     id=fleet_data.api_id,
                     name=fleet_data.api_name,
                     ship_ids=filter(lambda x: x != -1, fleet_data.api_ship),
-                    mission_id=mission_id))
+                    mission_id=mission_id,
+                    mission_complete=mission_complete))
         elif api_name == '/api_req_hensei/change':
             for i, data in enumerate(response['api_data']):
                 self.fleets[i].ship_ids = filter(lambda x: x != -1, data)
