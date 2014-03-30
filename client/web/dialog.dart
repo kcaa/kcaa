68138e9136304ac3480bfa9e3b9b8da6737a6707
KCAA Dev <kcaa.dev@gmail.com>
2014-03-31 01:20:26 +0900

    Make auto manipulator schedule dialog more human friendly.

--- client/web/dialog.dart
+++ client/web/dialog.dart
@@ -2,6 +2,7 @@ library kcaa_dialog;
 
 import 'dart:html';
 import 'package:polymer/polymer.dart';
+import 'package:intl/intl.dart';
 
 import 'assistant.dart';
 import 'model/assistant.dart';
@@ -24,21 +25,65 @@ class KcaaDialog extends PolymerElement {
   }
 }
 
+class FriendlyScheduleFragment extends Observable {
+  @observable String start;
+  @observable String end;
+
+  FriendlyScheduleFragment.empty() {
+    start = "0:00";
+    end = "0:00";
+  }
+
+  FriendlyScheduleFragment(ScheduleFragment fragment) {
+    var startHour = (fragment.start / 3600).toInt();
+    var startMin = ((fragment.start - 3600 * startHour) / 60).toInt();
+    var startMinPadding = startMin < 10 ? "0" : "";
+    start = "${startHour}:${startMinPadding}${startMin}";
+    var endHour = (fragment.end / 3600).toInt();
+    var endMin = ((fragment.end - 3600 * endHour) / 60).toInt();
+    var endMinPadding = endMin < 10 ? "0" : "";
+    end = "${endHour}:${endMinPadding}${endMin}";
+  }
+
+  ScheduleFragment toScheduleFragment() {
+    var startComponents = start.split(":");
+    var endComponents = end.split(":");
+    if (startComponents.length != 2 || endComponents.length != 2) {
+      throw new FormatException("Schedule fragments must have 2 components");
+    }
+    var startHour = int.parse(startComponents[0]);
+    var startMin = int.parse(startComponents[1]);
+    var endHour = int.parse(endComponents[0]);
+    var endMin = int.parse(endComponents[1]);
+    if (startHour < 0 || startMin < 0 || startMin >= 60 ||
+        endHour < 0 || endMin < 0 || endMin >= 60) {
+      throw new FormatException("Invalid range for hour:minute components");
+    }
+    return new ScheduleFragment(3600 * startHour + 60 * startMin,
+        3600 * endHour + 60 * endMin);
+  }
+}
+
 @CustomTag('kcaa-schedule-dialog')
 class ScheduleDialog extends KcaaDialog {
   @observable bool enabled;
-  @observable List<ScheduleFragment> schedules;
+  @observable final List<FriendlyScheduleFragment> schedules =
+      new ObservableList();
+  @observable String errorMessage;
 
   ScheduleDialog.created() : super.created();
 
   @override
   void show() {
     enabled = model.autoManipulatorsEnabled;
-    schedules = new ObservableList.from(model.autoManipulatorSchedules);
+    schedules.clear();
+    schedules.addAll(model.autoManipulatorSchedules.map(
+        (fragment) => new FriendlyScheduleFragment(fragment)));
+    errorMessage = null;
   }
 
   void addSchedule() {
-    schedules.add(new ScheduleFragment(0, 0));
+    schedules.add(new FriendlyScheduleFragment.empty());
   }
 
   void removeSchedule(MouseEvent e, var detail, Element target) {
@@ -46,8 +91,24 @@ class ScheduleDialog extends KcaaDialog {
     schedules.removeAt(index);
   }
 
+  void validateSchedule(Event e, var detail, Element target) {
+    try {
+      var dummy = new FriendlyScheduleFragment.empty();
+      dummy.start = (target as InputElement).value;
+      dummy.toScheduleFragment();
+      target.classes.remove("invalid");
+    } on FormatException {
+      target.classes.add("invalid");
+    }
+  }
+
   void ok() {
-    assistant.setAutoManipulatorSchedules(enabled, schedules);
-    close();
+    try {
+      assistant.setAutoManipulatorSchedules(enabled, schedules.map(
+          (fragment) => fragment.toScheduleFragment()));
+      close();
+    } on FormatException {
+      errorMessage = "時刻指定に誤りがあります。";
+    }
   }
 }
