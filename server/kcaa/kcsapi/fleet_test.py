df7496a7d4f1dc22c59133ff2736625295e9e59c
KCAA Dev <kcaa.dev@gmail.com>
2014-09-02 00:14:04 +0900

    Change the formatting of the text for jsonobject.parse_text().

--- server/kcaa/kcsapi/fleet_test.py
+++ server/kcaa/kcsapi/fleet_test.py
@@ -29,32 +29,30 @@ class TestFleetList(object):
         return fleet_list
 
     def test_update(self):
-        response = jsonobject.parse_text("""
-            {
-                "api_data": {
-                    "api_deck_port": [
-                        {
-                            "api_id": 1,
-                            "api_name": "FleetName",
-                            "api_ship": [
-                                123,
-                                456,
-                                789,
-                                -1,
-                                -1,
-                                -1
-                            ],
-                            "api_mission": [
-                                1,
-                                111,
-                                0,
-                                0
-                            ]
-                        }
-                    ]
-                }
+        response = jsonobject.parse_text("""{
+            "api_data": {
+                "api_deck_port": [
+                    {
+                        "api_id": 1,
+                        "api_name": "FleetName",
+                        "api_ship": [
+                            123,
+                            456,
+                            789,
+                            -1,
+                            -1,
+                            -1
+                        ],
+                        "api_mission": [
+                            1,
+                            111,
+                            0,
+                            0
+                        ]
+                    }
+                ]
             }
-        """)
+        }""")
         fleet_list = fleet.FleetList()
         fleet_list.update('/api_port/port', None, response, None, False)
         assert len(fleet_list.fleets) == 1
@@ -67,52 +65,44 @@ class TestFleetList(object):
 
     def test_update_ship_deployment(self, fleet_list):
         # Add the ship 4 to the end of the ship list.
-        request = jsonobject.parse_text("""
-            {
-                "api_id": "1",
-                "api_ship_idx": "3",
-                "api_ship_id": "4"
-            }
-        """)
+        request = jsonobject.parse_text("""{
+            "api_id": "1",
+            "api_ship_idx": "3",
+            "api_ship_id": "4"
+        }""")
         fleet_list.update(
             '/api_req_hensei/change', request, None, None, False)
         assert fleet_list.fleets[0].ship_ids == [1, 2, 3, 4]
 
     def test_update_ship_removal(self, fleet_list):
         # Remove the ship at the index 1 (the second ship).
-        request = jsonobject.parse_text("""
-            {
-                "api_id": "1",
-                "api_ship_idx": "1",
-                "api_ship_id": "-1"
-            }
-        """)
+        request = jsonobject.parse_text("""{
+            "api_id": "1",
+            "api_ship_idx": "1",
+            "api_ship_id": "-1"
+        }""")
         fleet_list.update(
             '/api_req_hensei/change', request, None, None, False)
         assert fleet_list.fleets[0].ship_ids == [1, 3]
 
     def test_update_fleet_clearance(self, fleet_list):
         # Remove all the ships except the flag ship.
-        request = jsonobject.parse_text("""
-            {
-                "api_id": "1",
-                "api_ship_idx": "0",
-                "api_ship_id": "-2"
-            }
-        """)
+        request = jsonobject.parse_text("""{
+            "api_id": "1",
+            "api_ship_idx": "0",
+            "api_ship_id": "-2"
+        }""")
         fleet_list.update(
             '/api_req_hensei/change', request, None, None, False)
         assert fleet_list.fleets[0].ship_ids == [1]
 
     def test_update_ship_swapping(self, fleet_list):
         # Swap the ship at the index 1 with the ship 3.
-        request = jsonobject.parse_text("""
-            {
-                "api_id": "1",
-                "api_ship_idx": "1",
-                "api_ship_id": "3"
-            }
-        """)
+        request = jsonobject.parse_text("""{
+            "api_id": "1",
+            "api_ship_idx": "1",
+            "api_ship_id": "3"
+        }""")
         fleet_list.update(
             '/api_req_hensei/change', request, None, None, False)
         assert fleet_list.fleets[0].ship_ids == [1, 3, 2]
@@ -120,13 +110,11 @@ class TestFleetList(object):
     def test_update_ship_swapping_between_fleets(self, fleet_list_2):
         # Swap the ship at the index 1 with the ship 5.
         # Note that the ship 5 is in the second fleet.
-        request = jsonobject.parse_text("""
-            {
-                "api_id": "1",
-                "api_ship_idx": "1",
-                "api_ship_id": "5"
-            }
-        """)
+        request = jsonobject.parse_text("""{
+            "api_id": "1",
+            "api_ship_idx": "1",
+            "api_ship_id": "5"
+        }""")
         fleet_list_2.update(
             '/api_req_hensei/change', request, None, None, False)
         assert fleet_list_2.fleets[0].ship_ids == [1, 5, 3]
