7d33c0638ef129b423bb0335e6f9a534f783e2a8
KCAA Dev <kcaa.dev@gmail.com>
2014-09-22 19:12:59 +0900

    Add "Delete SavedFleet" button.

--- client/web/dialog/fleet_organization_dialog.dart
+++ client/web/dialog/fleet_organization_dialog.dart
@@ -8,6 +8,8 @@ import '../model/assistant.dart';
 class FleetOrganizationDialog extends KcaaDialog {
   @observable SavedFleet fleet;
   @observable List<Ship> ships;
+  @observable String fleetNameToDelete;
+  @observable String errorMessage;
 
   @observable final String defaultClass = "";
   @observable final bool ignoreFilter = true;
@@ -23,5 +25,31 @@ class FleetOrganizationDialog extends KcaaDialog {
     ships = new ObservableList<Ship>.from(fleet.shipRequirements.map(
         (requirement) => model.shipMap[requirement.id]));
     debug = model.debug;
+
+    fleetNameToDelete = "";
+    errorMessage = null;
+    $["fleetNameToDelete"].classes.remove("invalid");
+  }
+
+  void ok() {
+    // TODO: Update saved fleet requirements.
+    close();
+  }
+
+  void delete() {
+    if (fleetNameToDelete != fleet.name) {
+      errorMessage = "確認のため、削除したい艦隊の名前を正確に入力してください。";
+      $["fleetNameToDelete"].classes.add("invalid");
+      return;
+    }
+    if (model.preferences.practicePrefs.practicePlans.any(
+        (practicePlan) => practicePlan.fleetName == fleet.name)) {
+      errorMessage = "この艦隊は演習計画に組み込まれています。先に計画から外してください。";
+      return;
+    }
+    model.preferences.fleetPrefs.savedFleets.removeWhere(
+        (savedFleet) => savedFleet == fleet);
+    assistant.savePreferences();
+    close();
   }
 }
