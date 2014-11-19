afa34cd115bfe5577f5d66ed185d0276622906d2
KCAA Dev <kcaa.dev@gmail.com>
2014-11-20 00:13:45 +0900

    Fix the bug where additional fields of a ship are reset when accessing KCSAPI ship3.

--- server/kcaa/kcsapi/ship.py
+++ server/kcaa/kcsapi/ship.py
@@ -527,10 +527,9 @@ class ShipList(model.KCAAObject):
             # /api_req_kaisou/remodeling.
             # When remodeling, ship3 only returns the updated ship, though as
             # a list.
-            ship_defs = objects['ShipDefinitionList'].ships
-            ship_data = response.api_data.api_ship_data
-            for data in ship_data:
-                ship = ship_defs[str(data.api_ship_id)].convert_to_dict()
+            # TODO: Check if get_ship() works with remodeling.
+            for data in response.api_data.api_ship_data:
+                ship = self.get_ship(data, objects).convert_to_dict()
                 ShipList.update_ship(ship, data)
                 self.ships[str(ship['id'])] = Ship(**ship)
         elif api_name == '/api_req_hensei/lock':
