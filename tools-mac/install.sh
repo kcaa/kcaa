4de4267cdf0fe07c29580f9bf21831847ed8faa9
KCAA Dev <kcaa.dev@gmail.com>
2014-12-18 17:14:30 +0900

    Add install/update scripts for Mac OS under tools-mac.

---
+++ tools-mac/install.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $0)
+source ${SCRIPT_DIR}/config
+
+PORT=$(which port)
+
+# WARNING: Mac OS X 10.9 (Marvericks) still use Bash v3!!
+
+function confirm_install_prerequisites() {
+local prerequisites=(gcc pip unzip curl)
+  which ${PORT} &> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Cannot find 'port' (MacPorts) at ${PORT}."
+    echo "Install it from https://www.macports.org/."
+    echo "TODO: Maybe support Homebrew as well."
+    exit 1
+  fi
+  for command in ${prerequisites[@]}
+  do
+    which ${command} &> /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Cannot find '${command}'."
+      echo "Follow the installation process at" \
+        "https://github.com/kcaa/kcaa#mac-os."
+      exit 1
+    fi
+  done
+}
+
+function create_user_data_directory() {
+  echo "Creating USER_DATA_DIR: ${USER_DATA_DIR} with mode 700"
+  mkdir -p -m 700 ${USER_DATA_DIR}
+  if [ $? -ne 0 ]; then
+    echo "Failed to create the user data path: ${USER_DATA_DIR}"
+    exit 1
+  fi
+}
+
+function install_kancolle_player_prerequisites() {
+  local kancolle_player_prerequisites=(
+  )
+  echo "Installing Kancolle player prerequisites..."
+  sudo ${PORT} install ${kancolle_player_prerequisites[@]}
+}
+
+# Python third-party packages required to run the KCAA Python server.
+function install_python_server_prerequisites() {
+  local python_server_macports_prerequisites=(
+  )
+#  echo "Installing KCAA Python server MacPorts prerequisites..."
+#  sudo ${PORT} install ${python_server_macports_prerequisites[@]}
+  local python_server_prerequisites=(
+    pillow
+    python-dateutil
+    requests
+    selenium
+    "--find-links https://code.google.com/p/google-visualization-python/ gviz-api-py"
+  )
+  echo "Installing KCAA Python server prerequisites..."
+  for package in "${python_server_prerequisites[@]}"
+  do
+    sudo pip install ${package}
+  done
+}
+
+# Python packages required to test the KCAA Python server code.
+function install_python_server_testing_prerequisites() {
+  local python_server_prerequisites=(
+    pytest
+    pytest-cov
+    pytest-pep8
+  )
+  echo "Installing KCAA Python server testing prerequisites..."
+  for package in ${python_server_testing_prerequisites[@]}
+  do
+    sudo pip install ${package}
+  done
+}
+
+function install_phantomjs() {
+  # PhantomJS requires some shared libraries to run.
+  local phantomjs_apt_prerequisites=(
+    python-pyphantomjs
+  )
+  echo "Installing PhantomJS APT prerequisites..."
+  sudo apt-get install ${phantomjs_apt_prerequisites[@]}
+}
+
+function install_dartium() {
+  if [ -d ${BIN_DIR}/dartium ]; then
+    echo "Dartium is already installed at ${BIN_DIR}/dartium. Skipping."
+    return
+  fi
+
+  local storage_base='http://storage.googleapis.com/dart-archive/channels/'\
+'stable/release/latest/dartium'
+  local filename='dartium-linux-x64-release.zip'
+  echo "Installing the latest Dartium..."
+  wget -q -O ${BIN_DIR}/${filename} ${storage_base}/${filename}
+  echo "Unzipping..."
+  unzip -q -d ${BIN_DIR} ${BIN_DIR}/${filename}
+  local dart_dir=$(unzip -l -qq ${BIN_DIR}/${filename} \
+    | awk '{print $NF}' \
+    | head -n 1 \
+    | sed -e 's|/$||')
+  ln -s ${BIN_DIR}/${dart_dir} ${BIN_DIR}/dartium
+}
+
+confirm_install_prerequisites
+create_user_data_directory
+#install_kancolle_player_prerequisites
+install_python_server_prerequisites
+#install_phantomjs
+#install_dartium
+
+UPDATE_REPOSITORY=0 ${SCRIPT_DIR}/update.sh
+
+echo "Installation finished."
---
+++ tools-mac/update.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Tricky -- no easy tool to canonicalize the path in Mac OS.
+KCAA_DIR=$(cd $(dirname $0)/..; pwd)
+UPDATE_REPOSITORY=${UPDATE_REPOSITORY-1}
+
+TMP_DIR=$(mktemp -d /tmp/tmp.XXXXXXXX)
+
+function update_repository() {
+  if ((UPDATE_REPOSITORY == 0)); then
+    return
+  fi
+  if [ ! -d .git ]; then
+    echo "No git repository found; you must have installed via a release" \
+      "package."
+    echo "In place update is not possible without git repository, as the" \
+      "server code will be inconsistent with client code. Aborting."
+    echo "If you want to enable in place update from next time, try git " \
+      "clone:"
+    echo "  cd ~"
+    echo "  git clone https://github.com/kcaa/kcaa.git"
+    echo "  cd kcaa"
+    echo "  tools/update.sh"
+    exit 1
+  fi
+  echo "Updating git repository..."
+  git fetch origin
+  git merge origin/latest_release
+  echo "Successfully updated git repository."
+}
+
+function update() {
+  local version_file=$1
+  local target_dir=$2
+
+  local current_version=v0.0.0
+  if [ -r ${target_dir}/${version_file} ]; then
+    current_version=$(head -n 1 ${target_dir}/${version_file} | cut -d , -f 1)
+  fi
+  local kcaa_repo_base=https://raw.githubusercontent.com/kcaa/kcaa
+  local latest_version_file=${kcaa_repo_base}/latest_release/${version_file}
+  curl -s ${latest_version_file} > ${TMP_DIR}/${version_file}
+  local latest_version=$(head -n 1 ${TMP_DIR}/${version_file} | cut -d , -f 1)
+  if [ "${current_version}" = "${latest_version}" ]; then
+    echo "${target_dir} is up to date. (${latest_version})"
+    return
+  fi
+  # Download and update the target directory.
+  if [ -d ${target_dir} ]; then
+    echo "Renaming the existing ${target_dir} to" \
+      "${target_dir}_${current_version}..."
+    mv ${target_dir} ${target_dir}_${current_version}
+  fi
+  echo "Downloading the latest ${target_dir} package of ${latest_version}..."
+  local latest_package=$(head -n 1 ${TMP_DIR}/${version_file} | cut -d , -f 2)
+  curl -s ${latest_package} > ${TMP_DIR}/${target_dir}.zip
+  unzip -q -d ${KCAA_DIR} ${TMP_DIR}/${target_dir}.zip
+  echo "Successfully updated ${target_dir} to ${latest_version}."
+}
+
+cd ${KCAA_DIR}
+
+update_repository
+update BINARY_VERSION bin
+update CLIENT_VERSION client_deployed
+
+rm -rf ${TMP_DIR}
+
+echo "Update finished."
