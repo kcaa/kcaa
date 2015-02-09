be9fbcb7cb79bfe5624abac5aa716726ca5f38ef
KCAA Dev <kcaa.dev@gmail.com>
2015-02-10 03:02:42 +0900

    Fix typo; boolean to bool.

--- client/web/model/assistant.dart
+++ client/web/model/assistant.dart
@@ -43,7 +43,7 @@ class AssistantModel extends Observable {
 
   // Fleets.
   @observable final List<Fleet> fleets = new ObservableList<Fleet>();
-  @observable boolean someFleetChargeable = false;
+  @observable bool someFleetChargeable = false;
   // TODO: Move this to somewhere appropriate. (ExpeditionPlan?)
   @observable KSelection formations = new KSelection.from(
       [["0", "単縦陣"],
